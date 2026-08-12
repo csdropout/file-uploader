6c6f3a8503e1707aeb7fcd1c7854e1ce2f00be8e
qile <98936712+csdropout@users.noreply.github.com>
2026-08-12 18:35:28 +0800

    Set locals.user with user

--- app.js
+++ app.js
@@ -35,6 +35,11 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
+
 app.get("/", (req, res) => {
   res.render("index");
 });
