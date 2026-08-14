ce9997e43474ac605ccf17159ce409f0911397fd
qile <98936712+csdropout@users.noreply.github.com>
2026-08-14 13:51:05 +0800

    fix: rename file value

--- public/scripts/fileItems.js
+++ public/scripts/fileItems.js
@@ -12,8 +12,8 @@ const renameFileButtons = document.querySelectorAll(".file-rename-btn");
 renameFileButtons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     e.stopPropagation();
-    const inputName = renameDialog.querySelector("input[name='name']");
-    const folderId = renameDialog.querySelector("input[name='id']");
+    const inputName = renameFileDialog.querySelector("input[name='name']");
+    const folderId = renameFileDialog.querySelector("input[name='id']");
 
     inputName.value = btn.dataset.name;
     folderId.value = btn.dataset.id;
